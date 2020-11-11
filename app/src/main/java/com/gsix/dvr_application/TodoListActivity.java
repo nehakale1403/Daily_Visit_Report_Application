6d0161d8afce0a7745ba208710d5036a22bd1be4
Darshan Deshmukh <darshandeshmukh14@gmail.com>
2020-11-11 10:59:26 +0530

    added Todo List

--- app/src/main/java/com/gsix/dvr_application/TodoListActivity.java
+++ app/src/main/java/com/gsix/dvr_application/TodoListActivity.java
@@ -1,14 +1,60 @@
 package com.gsix.dvr_application;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
-public class TodoListActivity extends AppCompatActivity {
+import com.gsix.dvr_application.Adapter.Todoadapter;
+import com.gsix.dvr_application.Model.ToDoModel;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class TodoListActivity extends AppCompatActivity {
+    private RecyclerView todoRecyclerView;
+    private Todoadapter tasksAdapter;
+
+    private List<ToDoModel> taskList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todo_list);
+
+        taskList = new ArrayList<>();
+
+        todoRecyclerView = findViewById(R.id.todoRecyclerView);
+        todoRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        tasksAdapter = new Todoadapter(this);
+        todoRecyclerView.setAdapter(tasksAdapter);
+
+        //dummy data
+
+        ToDoModel task = new ToDoModel();
+        task.setTask("This is a Test task");
+        task.setStatus(0);
+        task.setId(1);
+
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+
+        tasksAdapter.setTasks(taskList);
     }
 }
