31afd1989e2aa760dd136fc97dd6426824110828
Darshan Deshmukh <darshandeshmukh14@gmail.com>
2020-11-28 12:22:28 +0530

    Profile fetching data successfully

--- app/src/main/java/com/gsix/dvr_application/TodoListActivity.java
+++ app/src/main/java/com/gsix/dvr_application/TodoListActivity.java
@@ -55,7 +55,7 @@ public class TodoListActivity extends AppCompatActivity {
    @Override
    protected void onCreate(Bundle savedInstanceState) {
    super.onCreate(savedInstanceState);
-   getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//   getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
    setContentView(R.layout.activity_todo_list);
 
 
--- app/src/main/java/com/gsix/dvr_application/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/gsix/dvr_application/ui/profile/ProfileFragment.java
@@ -7,15 +7,18 @@ package com.gsix.dvr_application.ui.profile;
         import android.view.View;
         import android.view.ViewGroup;
         import android.widget.TextView;
+        import android.widget.Toast;
 
 
         import androidx.annotation.NonNull;
+        import androidx.annotation.Nullable;
         import androidx.fragment.app.Fragment;
 
 
 
         import com.google.firebase.auth.FirebaseAuth;
         import com.google.firebase.auth.FirebaseUser;
+        import com.google.firebase.database.ChildEventListener;
         import com.google.firebase.database.DataSnapshot;
         import com.google.firebase.database.DatabaseError;
         import com.google.firebase.database.DatabaseReference;
@@ -25,13 +28,15 @@ package com.gsix.dvr_application.ui.profile;
 
 public class ProfileFragment extends Fragment {
 
-    private TextView firstnametext, lastnametext, emailtext, mobiletext, empIDtext, addresstext;
+    private TextView firstnametext, emailtext, mobiletext, empIDtext, addresstext;
     private final String TAG = this.getClass().getName().toUpperCase();
-    private DatabaseReference reference;
+    private DatabaseReference reference,onlineCompanyIDref;
     private FirebaseAuth mAuth;
     private FirebaseUser mUser;
     private String onlineUserID;
-    private String onlineCompanyID;
+    public String onlineCompanyID;
+
+
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
@@ -40,45 +45,57 @@ public class ProfileFragment extends Fragment {
         mUser = mAuth.getCurrentUser();
         onlineUserID = mUser.getUid();
 
-        onlineCompanyID = FirebaseDatabase.getInstance().getReference().child("users").child(onlineUserID).child("CompanyId").toString();
-        reference = FirebaseDatabase.getInstance().getReference().child("Company").child(onlineCompanyID).child("Employees").child(onlineUserID).child("details");
-
-
         firstnametext = view.findViewById(R.id.profilefirstname);
-        lastnametext = view.findViewById(R.id.profilelastname);
         emailtext = view.findViewById(R.id.profileemail);
         mobiletext = view.findViewById(R.id.profilemobile);
         empIDtext = view.findViewById(R.id.profileempID);
         addresstext = view.findViewById(R.id.profileaddress);
 
 
+        onlineCompanyIDref = FirebaseDatabase.getInstance().getReference().child("users").child(onlineUserID).child("CompanyId");
 
-        //To read from firebase database
-        reference.addValueEventListener(new ValueEventListener() {
+
+        onlineCompanyIDref.addListenerForSingleValueEvent(new ValueEventListener()
+        {
             @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                for(DataSnapshot ds : dataSnapshot.getChildren()) {
-                        firstnametext.setText(ds.child("firstname").getValue(String.class));
-                        lastnametext.setText(ds.child("lastname").getValue(String.class));
-                        emailtext.setText(ds.child("email").getValue(String.class));
-                        mobiletext.setText(ds.child("mobile").getValue(String.class));
-                        empIDtext.setText(ds.child("empID").getValue(String.class));
-                        addresstext.setText(ds.child("address").getValue(String.class));
-                        break;
-
-                }
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Log.d("Company ID: ", snapshot.getValue().toString());
+                Log.d("Company ID key: ", snapshot.getKey());
+                onlineCompanyID = snapshot.getValue().toString();
+                reference = FirebaseDatabase.getInstance().getReference().child("Company")
+                        .child(onlineCompanyID).child("Employees").child(onlineUserID).child("details");
 
 
-            }
+
+                            reference.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            firstnametext.setText(snapshot.child("name").getValue(String.class));
+                            emailtext.setText(snapshot.child("email").getValue(String.class));
+                            mobiletext.setText(snapshot.child("mobile").getValue(String.class));
+                            empIDtext.setText(snapshot.child("empID").getValue(String.class));
+                            addresstext.setText(snapshot.child("address").getValue(String.class));
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+                        }
 
             @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-                // Failed to read value
-                Log.w(TAG, "Failed to read value.", databaseError.toException());
+            public void onCancelled(@NonNull DatabaseError error) {
+
             }
         });
 
 
+        firstnametext = view.findViewById(R.id.profilefirstname);
+        emailtext = view.findViewById(R.id.profileemail);
+        mobiletext = view.findViewById(R.id.profilemobile);
+        empIDtext = view.findViewById(R.id.profileempID);
+        addresstext = view.findViewById(R.id.profileaddress);
 
         return view;
     }
