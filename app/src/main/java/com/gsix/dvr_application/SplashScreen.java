dfeb532ae5adeb76825ada917d23a583f44e0534
mohit kumar jha <mohitjha727@gmail.com>
2020-12-05 17:22:26 +0530

    fixed splash screen

--- app/src/main/java/com/gsix/dvr_application/SplashScreen.java
+++ app/src/main/java/com/gsix/dvr_application/SplashScreen.java
@@ -9,7 +9,28 @@ public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Thread td= new Thread()
+        {
+            public void run(){
+                try {
+                    sleep(2000);
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+                finally {
+                    Intent intent= new Intent(getApplicationContext(), Login_Page.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    finish();
 
-        startActivity(new Intent(this,Login_Page.class));
+                }
+
+            }
+        };
+        td.start();
+        getSupportActionBar().hide();
     }
 }
