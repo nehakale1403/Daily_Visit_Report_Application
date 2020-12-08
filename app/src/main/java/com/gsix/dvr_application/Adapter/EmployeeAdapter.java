0e68c5afb23175fc6c66f49f4bcf9e889f759bcf
mohit kumar jha <mohitjha727@gmail.com>
2020-12-08 23:19:44 +0530

    fixed MyCheckinsFragment

--- app/src/main/java/com/gsix/dvr_application/Adapter/EmployeeAdapter.java
+++ app/src/main/java/com/gsix/dvr_application/Adapter/EmployeeAdapter.java
@@ -4,6 +4,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -45,7 +46,6 @@ public class EmployeeAdapter extends RecyclerView.Adapter<EmployeeAdapter.ViewHo
         holder.empname.setText(checkin.getName());
         holder.empcheckin.setText(checkin.getEmpID());
         holder.ranking.setText(String.valueOf(position+1));
-
     }
 
     @Override
